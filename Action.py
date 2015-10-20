f6c5f26ac97b45f74ba0ffded4d9f2684a4ef98c
Jason Liu <liujas000@gmail.com>
2015-10-19 22:39:29 -0700

    added util.py

---
+++ Action.py
@@ -0,0 +1,6 @@
+class Action:
+
+	def canPerform(player):
+		return None
+	def go(player1, player2 = None):
+		return None
---
+++ util.py
@@ -0,0 +1,27 @@
+characterList = ['ambassador', 'assassin', 'captain', 'contessa', 'duke']
+
+characterToAction = {
+	
+	'ambassador': 'exchange',
+	'assassin': 'assassinate',
+	'captain' : 'steal',
+	'duke': 'tax'
+}
+
+#actionToCharacter
+#blockToCharacter
+
+
+characterToBlock = {
+	
+	'ambassador' : 'steal',
+	'captain' : 'steal',
+	'contessa' : 'assassinate',
+	'duke:' : 'foreignAid'
+}
+
+basicActions = ['income', 'foreignAid', 'coup']
+specialActions = ['steal', 'assassinate', 'exchange', 'tax']
+blocks = ['steal', 'assassinate', 'foreignAid']
+
+
